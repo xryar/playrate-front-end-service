67eb77132f075003595184322bdbd15cf77ef045
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-22 12:21:21 +0700

    setup home page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,10 +1,16 @@
-import { checkUnauthenticatedRouteOnly } from "../utils/auth";
+import {
+  checkAuthenticatedRoute,
+  checkUnauthenticatedRouteOnly,
+} from "../utils/auth";
 import LoginPage from "../pages/auth/login/login-page";
 import RegisterPage from "../pages/auth/register/register-page";
+import HomePage from "../pages/home/home-page";
 
 const routes = {
   "/login": () => checkUnauthenticatedRouteOnly(new LoginPage()),
   "/register": () => checkUnauthenticatedRouteOnly(new RegisterPage()),
+
+  "/": () => checkAuthenticatedRoute(new HomePage()),
 };
 
 export default routes;
