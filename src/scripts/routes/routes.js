cf8c8a6c92cfb19dbc6726bbe42452ad747421ef
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-22 12:13:43 +0700

    setup login and register

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,8 @@
+import {checkUnauthenticatedRouteOnly} from "../utils/auth";
+import LoginPage from "../pages/auth/login/login-page";
+import RegisterPage from "../pages/auth/register/register-page";
+
+const routes = {
+    '/login': () => checkUnauthenticatedRouteOnly(new LoginPage()),
+    '/register': () => checkUnauthenticatedRouteOnly(new RegisterPage()),
+}
