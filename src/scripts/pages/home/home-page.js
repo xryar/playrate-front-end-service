497cecf0f8daac33bea9a17abea235ab06f07ea5
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-23 10:31:06 +0700

    update home page

--- src/scripts/pages/home/home-page.js
+++ src/scripts/pages/home/home-page.js
@@ -1,5 +1,10 @@
 import HomePresenter from "./home-presenter";
 import * as ReviewsAPI from "../../data/api";
+import {
+    generateLoaderAbsoluteTemplate, generateReviewItemTemplate,
+    generateReviewsListEmptyTemplate,
+    generateReviewsListErrorTemplate
+} from "../../template";
 
 export default class HomePage {
     #presenter;
@@ -24,5 +29,45 @@ export default class HomePage {
         });
 
         await this.#presenter.initialReviews();
+    }
+
+    populateReviewsList(message, listReviews) {
+        if (listReviews.left <= 0) {
+            this.populateReviewsListEmpty();
+            return;
+        }
+
+        const html = listReviews.reduce((acc, review) => {
+            return acc.concat(
+                generateReviewItemTemplate({
+                    id: review.id,
+                    username: review.username,
+                    title: review.title,
+                    description: review.description,
+                    coverUrl: review.coverUrl,
+                    createdAt: review.createdAt,
+                })
+            );
+        }, '');
+
+        document.getElementById("reviews-list").innerHTML = `
+            <div class="reviews-list">${html}</div>
+        `;
+    }
+
+    populateReviewsListEmpty() {
+        document.getElementById("reviews-list").innerHTML = generateReviewsListEmptyTemplate();
+    }
+
+    populateReviewsListError(message) {
+        document.getElementById("reviews-list").innerHTML = generateReviewsListErrorTemplate(message);
+    }
+
+    showLoading() {
+        document.getElementById("reviews-list-loading-container").innerHTML = generateLoaderAbsoluteTemplate();
+    }
+
+    hideLoading() {
+        document.getElementById("reviews-list-loading-container").innerHTML = "";
     }
 }
