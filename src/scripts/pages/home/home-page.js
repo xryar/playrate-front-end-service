5b0069b089fcbca4fcfa82385dcefd4df7dbdaff
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-23 10:21:00 +0700

    setup homepage

--- src/scripts/pages/home/home-page.js
+++ src/scripts/pages/home/home-page.js
@@ -1,1 +1,28 @@
-export default class HomePage {}
+import HomePresenter from "./home-presenter";
+import * as ReviewsAPI from "../../../data/api";
+
+export default class HomePage {
+    #presenter;
+
+    async render() {
+        return `
+          <section class="container">
+            <h1 class="section-title">List Review</h1>
+            
+            <div class="reviews-list__container">
+                <div id="reviews-list"></div>
+                <div id="reviews-list-loading-container"></div>
+            </div>
+          </section>
+        `;
+    }
+
+    async afterRender() {
+        this.#presenter = new HomePresenter({
+            view: this,
+            model: ReviewsAPI,
+        });
+
+        await this.#presenter.initialReviews();
+    }
+}
